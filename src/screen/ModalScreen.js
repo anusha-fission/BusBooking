9a3fb652603f5ee813480c273f44d6b313955fd1
Vikash-Yadav79 <vikash.yadav@fissionlabs.com>
2022-10-17 17:34:56 +0530

    Ui fixed

--- src/screen/ModalScreen.js
+++ src/screen/ModalScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {Button, View, Text, Modal, TextInput, StyleSheet} from 'react-native';
+import {
+  Button,
+  ScrollView,
+  View,
+  Text,
+  Modal,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
 
 class ModalScreen extends React.Component {
   constructor() {
@@ -10,19 +18,19 @@ class ModalScreen extends React.Component {
   }
   render() {
     return (
-      <View>
-        <Modal transparent={true} visible={this.state.show}>
-          <View style={styles.container} />
+      <Modal transparent={true} visible={this.state.show}>
+        <View style={[styles.container]} />
+        <View
+          style={[
+            styles.modal,
+            {
+              top: this.state.showSuccess ? '30%' : '10%',
+              bottom: this.state.showSuccess ? '30%' : '10%',
+            },
+          ]}>
           {this.state.showSuccess ? (
-            <View
-              style={{
-                position: 'absolute',
-                top: 140,
-                zIndex: 1,
-                left: 120,
-                marginTop: 100,
-              }}>
-              <Text style={{fontSize: 40}}>Successs !</Text>
+            <View style={{alignItems: 'center'}}>
+              <Text style={{fontSize: 24, marginBottom: 30}}>Successs !</Text>
               <Button
                 title="Okay"
                 onPress={() => {
@@ -31,88 +39,95 @@ class ModalScreen extends React.Component {
               />
             </View>
           ) : (
-            <View style={{position: 'absolute', top: 40, zIndex: 1, left: 20}}>
-              <TextInput
-                style={styles.form}
-                placeholder="number of seat"
-                onChange={text => {
-                  this.setState({email: text});
-                }}
-              />
-              <TextInput
-                style={styles.form}
-                placeholder="Enter Email"
-                onChange={text => {
-                  this.setState({email: text});
-                }}
-              />
-              <TextInput
-                style={styles.form}
-                placeholder="phone number"
-                onChange={text => {
-                  this.setState({email: text});
-                }}
-              />
-              <TextInput
-                style={styles.form}
-                placeholder=" enter Age"
-                onChange={text => {
-                  this.setState({email: text});
-                }}
-              />
-              <TextInput
-                style={styles.form}
-                placeholder=" Enter Gender"
-                onChange={text => {
-                  this.setState({email: text});
-                }}
-              />
-              <View
-                style={{position: 'absolute', top: 480, zIndex: 1, left: 20}}>
-                <Button
-                  title="Cancel Ticket"
-                  onPress={() => {
-                    this.props.setShowModal(false);
+            <View>
+              <ScrollView>
+                <TextInput
+                  style={styles.form}
+                  placeholder="number of seat"
+                  onChange={text => {
+                    this.setState({email: text});
                   }}
                 />
-              </View>
+                <TextInput
+                  style={styles.form}
+                  placeholder="Enter Email"
+                  onChange={text => {
+                    this.setState({email: text});
+                  }}
+                />
+                <TextInput
+                  style={styles.form}
+                  placeholder="phone number"
+                  onChange={text => {
+                    this.setState({email: text});
+                  }}
+                />
+                <TextInput
+                  style={styles.form}
+                  placeholder=" enter Age"
+                  onChange={text => {
+                    this.setState({email: text});
+                  }}
+                />
+                <TextInput
+                  style={styles.form}
+                  placeholder=" Enter Gender"
+                  onChange={text => {
+                    this.setState({email: text});
+                  }}
+                />
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-around',
+                  }}>
+                  <Button
+                    title="Cancel Ticket"
+                    onPress={() => {
+                      this.props.setShowModal(false);
+                    }}
+                  />
 
-              <View
-                style={{position: 'absolute', top: 480, zIndex: 1, left: 220}}>
-                <Button
-                  title="Book Ticket"
-                  onPress={() => {
-                    this.setState({showSuccess: true});
-                  }}
-                />
-              </View>
+                  <Button
+                    title="Book Ticket"
+                    onPress={() => {
+                      this.setState({showSuccess: true});
+                    }}
+                  />
+                </View>
+              </ScrollView>
             </View>
           )}
-        </Modal>
-      </View>
+        </View>
+      </Modal>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#E6E6FA',
+    backgroundColor: 'rgba(0,0,0,0.5)',
     alignItems: 'center',
     justifyContent: 'center',
-    width: '90%',
-    height: '80%',
+    flex: 1,
+  },
+  modal: {
+    position: 'absolute',
+    left: 20,
+    right: 20,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#fff',
     paddingHorizontal: 20,
     paddingVertical: 30,
-    marginLeft: 20,
-    marginTop: 20,
-    borderRadious: 50,
+    borderRadius: 16,
   },
   form: {
     borderWidth: 2,
     margin: 20,
     color: 'blue',
-    borderRadius: 20,
-    width: '150%',
+    borderRadius: 8,
     paddingHorizontal: 10,
     fontSize: 20,
     borderColor: 'red',
